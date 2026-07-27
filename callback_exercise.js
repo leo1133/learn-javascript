cf1749d52467a8680c5e001b8df082dfb7d5f520
leo1133 <ngaphuongtb123@gmail.com>
2026-07-27 22:55:03 +0700

    callback exercise part 2

--- callback_exercise.js
+++ callback_exercise.js
@@ -37,7 +37,7 @@ Array.prototype.myMap = function (cb) {
 };
 
 // Expected results
-const numbers = [1, 2, 3];
+var numbers = [1, 2, 3];
 
 console.log(
   numbers.myMap(function (number) {
@@ -50,3 +50,108 @@ console.log(
     return number * index;
   }),
 ); // Output: [0, 2, 6]
+
+//Bài 3: tạo ra phương thức myFilter hoạt động tương tự phương thức filter2 và filter
+Array.prototype.myFilter = function (cb) {
+  var newArray = [];
+
+  for (var i = 0; i < this.length; i++) {
+    if (cb(this[i], i, this)) {
+      newArray.push(this[i]);
+    }
+  }
+
+  return newArray;
+};
+
+// Expected results:
+var numbers = [1, 2, 3, 4];
+
+console.log(
+  numbers.myFilter(function (number) {
+    return number % 2 === 0;
+  }),
+);
+Output: [2, 4];
+
+console.log(
+  numbers.myFilter(function (number, index) {
+    return index % 2 === 0;
+  }),
+);
+Output: [1, 3];
+
+console.log(
+  numbers.myFilter(function (number, index, array) {
+    return array.length % 2 === 0;
+  }),
+);
+Output: [1, 2, 3, 4];
+
+//Bài 4: tạo ra phương thức mySome hoạt động tương tự phương thức some2 và some
+Array.prototype.mySome = function (cb) {
+  for (var i = 0; i < this.length; i++) {
+    if (cb(this[i], i, this)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+// Expected results:
+
+var numbers = [1, 3, 3, 5];
+
+console.log(
+  numbers.mySome(function (number) {
+    return number % 2 === 0;
+  }),
+);
+Output: false;
+
+console.log(
+  numbers.mySome(function (number, index) {
+    return index % 2 === 0;
+  }),
+);
+Output: true;
+
+console.log(
+  numbers.mySome(function (number, index, array) {
+    return array.length % 2 === 0;
+  }),
+);
+Output: true;
+
+//Bài 5: tạo ra phương thức myEvery hoạt động tương tự phương thức every2 và every
+Array.prototype.myEvery = function (cb) {
+  for (var i = 0; i < this.length; i++) {
+    if (cb(this[i], i, this) === false) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Expected results
+
+var numbers = [1, 3, 3, 5];
+
+console.log(
+  numbers.myEvery(function (number) {
+    return number % 2 !== 0;
+  }),
+); // Output: true
+
+console.log(
+  numbers.myEvery(function (number, index) {
+    return index % 2 === 0;
+  }),
+); // Output: false
+
+console.log(
+  numbers.myEvery(function (number, index, array) {
+    return array.length % 2 === 0;
+  }),
+); // Output: true
