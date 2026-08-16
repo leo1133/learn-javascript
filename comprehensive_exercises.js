a350af83157e80d8c9a3220b55e1a55fd922aba4
leo1133 <ngaphuongtb123@gmail.com>
2026-08-16 15:09:22 +0700

    update exercise

--- comprehensive_exercises.js
+++ comprehensive_exercises.js
@@ -58,3 +58,45 @@ function remainder(x, y) {
   return result;
 }
 console.log(remainder(17, 9));
+
+// Bài 10: Hãy tạo một hàm nhận hai số làm đối số và trả về tổng của chúng.
+function addition(a, b) {
+  var result = a + b;
+  return result;
+}
+console.log(addition(3, 2));
+
+// Bài 11: Hãy tạo một hàm nhận vào một mảng các số và trả về cả số nhỏ nhất và số lớn nhất, theo đúng thứ tự đó.
+function minMax(arr) {
+  var result = [Math.min(...arr), Math.max(...arr)];
+  return result;
+}
+console.log(minMax([2, 3, 6, 9, 10, 5, 88]));
+
+// Bài 12: Hãy viết một hàm nhận vào cạnh đáy và chiều cao của một tam giác để tính diện tích của nó.
+function triArea(base, height) {
+  var result = (base * height) / 2;
+  return result;
+}
+console.log(triArea(3, 2));
+
+// Bài 13: Hãy tạo một hàm nhận vào một giá trị length và width một số hạng, rồi tìm chu vi của hình chữ nhật.
+function findPerimeter(length, width) {
+  var result = (length + width) * 2;
+  return result;
+}
+console.log(findPerimeter(10, 20));
+
+// Bài 14: Hãy tạo một hàm nhận vào một mảng chỉ chứa các số và trả về phần tử đầu tiên.
+function getFirstValue(arr) {
+  var result = arr[0];
+  return result;
+}
+console.log(getFirstValue([1, 5, 9, 0, 2]));
+
+// Bài 15: Hãy tạo một hàm trả về số lượng true phần tử có trong một mảng.
+function countTrue(arr) {
+  var result = arr.filter((value) => value === true);
+  return result.length;
+}
+console.log(countTrue([true, false, false, true, false]));
